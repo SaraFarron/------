6692eed29d40a03ce0330ab62d816519eb279763
SaraFarron <egu7pC6bsPtPpDz>
2022-03-24 22:58:28 +0700

    ftp update

--- python/ftp.py
+++ python/ftp.py
@@ -1,2 +1,19 @@
 from utils import *
 
+RINEX_FILES_DIRS = [
+    'gnss/data/daily/2020/001/20q/',
+    'gnss/data/daily/2020/001/20p/',
+    'gnss/data/daily/2020/001/20n/',
+    'gnss/data/daily/2020/001/20m/',
+    'gnss/data/daily/2020/001/20l/',
+    'gnss/data/daily/2020/001/20h/',
+    'gnss/data/daily/2020/001/20g/',
+    'gnss/data/daily/2020/001/20f/',
+]
+
+"""
+TODO create ftp_data folder, download files there with saving tree structure. Download rinexes from l folder, 
+check for cords + ionospheric coefs
+"""
+
+def get_rinex_files(): pass
