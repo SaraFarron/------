f12025777c14e5b483efe6d5d1ef24067f0e6632
SaraFarron <egu7pC6bsPtPpDz>
2022-04-26 11:44:21 +0700

    added working build

--- python/nequick.py
+++ python/nequick.py
@@ -6,9 +6,9 @@ NEQUICK_MODIP_ARG = r'C:\NeQuickJRC\NeQuickJRC\modip\modip2001_wrapped.asc '
 NEQUICK_CCIR_ARG = r'C:\NeQuickJRC\NeQuickJRC\ccir\ '
 
 a0, a1, a2 = 2.4500E+01, 3.2422E-01, 4.3640E-03
-time = '02 18:24:27'
-station_cords = [-15.425538589403404, 28.311014636811894, 1324.9196041869]
-sattelite_cords = [-111.18083374029375, 137.59726265257754, -6401946.81655583]
+time = '02 18.287643'
+station_cords = [28.311014636811894, -15.425538589403404, 1324.9196041869]
+sattelite_cords = [137.5972626525775, -64.47973242387415, -6374528.453041455]
 
 gal_args = ' '.join(map(str, [a0, a1, a2])) + ' '
 time_args = time + ' '
